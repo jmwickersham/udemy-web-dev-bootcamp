77602c4f95b7c9ee6a99d4a484f05550b3ee6b22
Jennifer Wickersham <jennifermwickersham@gmail.com>
2017-11-26 23:41:35 -0800

    JS - Section 8
    First section of JS exercises

---
+++ JS/stalkerExercise.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+// can also create a variable to concatonate the fullName
+// var fullName = firstName + " " + lastName;
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
