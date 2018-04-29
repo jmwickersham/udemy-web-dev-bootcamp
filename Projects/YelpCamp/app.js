b8ef5d4319f0e82d4cf11c344a9775a6ade374d6
Jen <jennifermwickersham@gmail.com>
2018-04-29 07:14:00 +0000

    update database url

--- Projects/YelpCamp/app.js
+++ Projects/YelpCamp/app.js
@@ -50,8 +50,9 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://admin:password@ds161529.mlab.com:61529/yelp-camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://admin:password@ds161529.mlab.com:61529/yelp-camp");
+mongoose.connect(process.env.DATABASEURL);
 
 // seedDB(); // Seed the database
 
